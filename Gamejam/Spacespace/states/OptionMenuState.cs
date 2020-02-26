a2dc99d3e17efe4aa5e346b76d564fe231369f50
nasr250 <56081094+nasr250@users.noreply.github.com>
2020-02-26 13:48:24 +0100

    cam nu helemaal loesoe

--- Gamejam/Spacespace/states/OptionMenuState.cs
+++ Gamejam/Spacespace/states/OptionMenuState.cs
@@ -33,32 +33,6 @@ class OptionMenuState : GameObjectList
         backButton.Position = new Vector2(GameEnvironment.Screen.X - backButton.Width, 10);
         Add(backButton);
 
-        for (int i = 0; i < GameEnvironment.Camera.Devices.Count; i++)
-            cameraNames.Add(GameEnvironment.Camera.Devices[i].Name);
-        cameraSelectionButton = new DropDownButton(GameEnvironment.Camera.Devices.Count, cameraNames);
-        cameraSelectionButton.Position = new Vector2(10, 500);
-        Add(cameraSelectionButton);
-
-        Red = new SliderButton("Sprites/spr_button_sliderback", "Sprites/spr_button_sliderfront");
-        Add(Red);
-        Red.Position = new Vector2(800, 300);
-        Red.Value = (float)GameEnvironment.Camera.FilterColor.Red/255;
-
-        Blue = new SliderButton("Sprites/spr_button_sliderback", "Sprites/spr_button_sliderfront");
-        Add(Blue);
-        Blue.Position = new Vector2(800, 420);
-        Blue.Value = (float)GameEnvironment.Camera.FilterColor.Blue/255;
-
-        Green = new SliderButton("Sprites/spr_button_sliderback", "Sprites/spr_button_sliderfront");
-        Add(Green);
-        Green.Position = new Vector2(800, 360);
-        Green.Value = (float)GameEnvironment.Camera.FilterColor.Green/255;
-
-        Sensitivity = new SliderButton("Sprites/spr_button_sliderback", "Sprites/spr_button_sliderfront");
-        Add(Sensitivity);
-        Sensitivity.Position = new Vector2(800, 480);
-        Sensitivity.Value = (float)GameEnvironment.Camera.FilterSensitivity/255;
-
         Volume = new SliderButton("Sprites/spr_button_sliderback", "Sprites/spr_button_sliderfront");
         Add(Volume);
         Volume.Position = new Vector2(1350, 300);
@@ -77,62 +51,20 @@ class OptionMenuState : GameObjectList
     {
         base.Update(gameTime);
 
-        if(GameEnvironment.Camera.frame != null)
-        {
-            Red.Position = new Vector2(200 + GameEnvironment.Camera.frame.Width, 300);
-            Blue.Position = new Vector2(200 + GameEnvironment.Camera.frame.Width, 420);
-            Green.Position = new Vector2(200 + GameEnvironment.Camera.frame.Width, 360);
-            Sensitivity.Position = new Vector2(200 + GameEnvironment.Camera.frame.Width, 480);
-            Volume.Position = new Vector2(200 + GameEnvironment.Camera.frame.Width, 600);
-
-            cameraSelectionButton.Position = new Vector2(10, 20 + GameEnvironment.Camera.frame.Height);
-            colorFilterToggle.Position = new Vector2(260, 20 + GameEnvironment.Camera.frame.Height);
-
-        }
 
 
-        //Changes the camera when a different one is selected
-        if (selectedCamera != cameraSelectionButton.selectedOption)
-        {
-            selectedCamera = cameraSelectionButton.selectedOption;
-
-            GameEnvironment.Camera.SwitchWebcam(selectedCamera);
-        }
-
-        RedText = ("Red: " + ((int)(Red.Value * 255)).ToString());
-        GameEnvironment.Camera.ChangefilterRed((int)(Red.Value * 255));
-
-        BlueText = ("Blue: " + ((int)(Blue.Value * 255)).ToString());
-        GameEnvironment.Camera.ChangefilterBlue((int)(Blue.Value * 255));
-
-        GreenText = ("Green: " + ((int)(Green.Value * 255)).ToString());
-        GameEnvironment.Camera.ChangefilterGreen((int)(Green.Value * 255));
-
-        SensitivityText = ("Sensitivity: " + ((int)(Sensitivity.Value * 255)).ToString());
-        GameEnvironment.Camera.ChangefilterSensitivity((int)(Sensitivity.Value * 255));
-
-        VolumeText = ("Volume: " + ((int)(Volume.Value * 100)).ToString());
-        MediaPlayer.Volume = Volume.Value / 10;
-
-        if (colorFilterToggle.ToggledOn)
-            GameEnvironment.Camera.SwitchCameraMode(Camera.CameraStatus.Tracking);
-        else GameEnvironment.Camera.SwitchCameraMode(Camera.CameraStatus.Active);
+      
     }
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         base.Draw(gameTime, spriteBatch);
 
-        GameEnvironment.Camera.Draw(gameTime, spriteBatch);
         spriteBatch.Draw(TrackedColor, new Vector2(900, 230), new Color(Red.Value, Green.Value, Blue.Value));
         spriteBatch.DrawString(spriteFont, RedText, Red.Position - new Vector2(83, -8), Color.White);
         spriteBatch.DrawString(spriteFont, BlueText, Blue.Position - new Vector2(85, -8), Color.White);
         spriteBatch.DrawString(spriteFont, GreenText, Green.Position - new Vector2(100, -8), Color.White);
         spriteBatch.DrawString(spriteFont, SensitivityText, Sensitivity.Position - new Vector2(130, -8), Color.White);
         spriteBatch.DrawString(spriteFont, VolumeText, Volume.Position - new Vector2(112, -8), Color.White);
-        spriteBatch.Draw(TrackedObject, new Rectangle(GameEnvironment.Camera.BlobRectangle.X + (int)GameEnvironment.Camera.Position.X,
-                                                      GameEnvironment.Camera.BlobRectangle.Y + (int)GameEnvironment.Camera.Position.Y,
-                                                      GameEnvironment.Camera.BlobRectangle.Width, GameEnvironment.Camera.BlobRectangle.Height),
-                                                      Color.White);
     }
 
     public override void HandleInput(InputHelper inputHelper)
@@ -140,7 +72,6 @@ class OptionMenuState : GameObjectList
         base.HandleInput(inputHelper);
         if (backButton.Pressed)
         {
-            GameEnvironment.Camera.SwitchCameraMode(Camera.CameraStatus.Tracking);
             GameEnvironment.GameStateManager.SwitchTo("titleMenu");
         }
     }
