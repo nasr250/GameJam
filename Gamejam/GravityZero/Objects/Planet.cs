a786279c21ed9481d83426314bb94251a213800b
AlexC <aleksej.cornelissen@hotmail.com>
2020-03-11 14:09:26 +0100

    bullet gravity and despawn timer

--- Gamejam/GravityZero/Objects/Planet.cs
+++ Gamejam/GravityZero/Objects/Planet.cs
@@ -36,7 +36,51 @@ public class Planet : AnimatedGameObject
                 player.Velocity += -G * mass * unit / Vector2.DistanceSquared(position, player.Position);
             }
         }
+        GameObjectList bullets = GameWorld.Find("bullets") as GameObjectList;
+        for (int i = bullets.children.Count - 1; i >= 0; i--)
+        {
+            SpriteGameObject bullet = bullets.children[i] as SpriteGameObject;
+            if (CollidesWith(bullet))
+            {
+                bullets.children[i].health--;
+                this.health--;
+                if (bullets.children[i].health < 0)
+                {
+                    TextGameObject kills = GameWorld.Find("killsText") as TextGameObject;
+                    kills.health++;
+                }
+            }
+            if (!CollidesWith(bullet))
+            {
+                Vector2 uv = bullet.Position - position;
+                Vector2 unit = uv / Vector2.Distance(position, bullet.Position);
+                bullet.Velocity += -G * mass * unit / Vector2.DistanceSquared(position, bullet.Position);
+            }
 
+        }
+
+        GameObjectList friendlyBullets = GameWorld.Find("friendlyBullets") as GameObjectList;
+        for (int i = friendlyBullets.children.Count - 1; i >= 0; i--)
+        {
+            SpriteGameObject friendlyBullet = friendlyBullets.children[i] as SpriteGameObject;
+            if (CollidesWith(friendlyBullet))
+            {
+                friendlyBullets.children[i].health--;
+                this.health--;
+                if (friendlyBullets.children[i].health < 0)
+                {
+                    TextGameObject kills = GameWorld.Find("killsText") as TextGameObject;
+                    kills.health++;
+                }
+            }
+            if (!CollidesWith(friendlyBullet))
+            {
+                Vector2 uv = friendlyBullet.Position - position;
+                Vector2 unit = uv / Vector2.Distance(position, friendlyBullet.Position);
+                friendlyBullet.Velocity += -G * mass * unit / Vector2.DistanceSquared(position, friendlyBullet.Position);
+            }
+
+        }
 
     }
 
