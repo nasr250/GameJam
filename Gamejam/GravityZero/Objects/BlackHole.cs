928ea1af69c737c3e2e7911e31a8d2216d61f96d
Joeri Dukker <joeridukker3@gmail.com>
2020-03-15 18:31:04 +0100

    c

--- Gamejam/GravityZero/Objects/BlackHole.cs
+++ Gamejam/GravityZero/Objects/BlackHole.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 class BlackHole : Planet
 {
+    static  Vector2 poss = new Vector2(2000, 10);
     public BlackHole() : base("Sprites/blackhole@1x1")
     {
-        mass = 1000;
+        //mass = 1000;
     }
 }
