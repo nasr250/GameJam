39c004458aa4dab9d405b2c156617e0f1504fa4f
r2dvos <voswesseling@gmail.com>
2020-03-18 17:29:29 +0100

    u

--- Gamejam/GravityZero/GameManagement/Player.cs
+++ Gamejam/GravityZero/GameManagement/Player.cs
@@ -198,12 +198,12 @@ public class Player : AnimatedGameObject
                 bar.size -= (500 - reducedFuelCost);
         }
 
-        if (inputHelper.KeyPressed(Keys.U)) // debug: temp power up switch
+        /*if (inputHelper.KeyPressed(Keys.U)) // debug: temp power up switch
         {
             if (powerUpState <= 3)
                 powerUpState++;
             else powerUpState = 0;
-        }
+        }*/
 
     }
 
